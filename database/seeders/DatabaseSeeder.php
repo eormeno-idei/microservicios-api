4ded9ff779f32caa09536578ce08348dc0669989
Emilio Gustavo Ormeno <eormeno@iinfo.unsj.edu.ar>
2025-10-22 19:07:16 -0300

    feat: Add PostSeeder to seed posts with relationships and validation

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,7 @@ class DatabaseSeeder extends Seeder
             UserSeeder::class,
             ChannelSeeder::class,
             MediaSeeder::class,
+            PostSeeder::class,
         ]);
     }
 }
