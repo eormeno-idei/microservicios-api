adb7dee4e4fb19e11d98bb24993788934ef90e52
Emilio Ormeño <eormeno@gmail.com>
2026-01-29 12:26:11 -0300

    Fix: Register ForgotPasswordService in UI Config (Bugfix #8)

--- config/ui-services.php
+++ config/ui-services.php
@@ -28,6 +28,8 @@ return [
     */
 
     \App\Services\Screens\DemoUiService::class,
+    \App\Services\Screens\ForgotPasswordService::class,
+    \App\Services\Screens\LoginService::class,
     \App\Services\Screens\InputDemoService::class,
     \App\Services\Screens\SelectDemoService::class,
     \App\Services\Screens\CheckboxDemoService::class,
